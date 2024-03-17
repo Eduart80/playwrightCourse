c3d17a6532ce81f7f31121adfa8642a20980ce59
Eduart2021 <zeedymarina@yahoo.com>
2024-03-16 23:12:33 -0500

    api2

--- apiCalls/getLogToken.js
+++ apiCalls/getLogToken.js
@@ -3,11 +3,13 @@ import * as nodeFetch from 'node-fetch';
 export const getToken = async ()=>{
     const respond = await nodeFetch('http://localhost:2221/api/login', {
         method: 'POST',
-        body: JSON.stringify({"username":"test@test.com", "password":"1234"})
+        body: JSON.stringify({"username":"test@test.com", "password": process.env.PASSWORD
+    })
     })
     if (respond.status !== 200) {
         throw new Error("An error occured trying to retrieve the login token.")
     }
+    console.log('code: '+respond.status);
     const res = await respond.json();
     return res.token
 }
